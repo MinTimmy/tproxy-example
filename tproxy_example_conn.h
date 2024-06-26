fc3471dd9b289f8d1cb3ab3285b2df0f848de460
MinTimmy <timmygood6590@gmail.com>
2024-06-26 19:31:07 +0800

    init

---
+++ tproxy_example_conn.h
@@ -0,0 +1,12 @@
+//Written by Kristian Evensen <kristian.evensen@gmail.com>
+
+#ifndef TPROXY_TEST_CONN_H
+#define TPROXY_TEST_CONN_H
+
+#include "tproxy_example.h"
+
+tproxy_conn_t* add_tcp_connection(int efd, struct tailhead *conn_list, 
+        int local_fd);
+void free_conn(tproxy_conn_t *conn);
+int8_t check_connection_attempt(tproxy_conn_t *conn, int efd);
+#endif
